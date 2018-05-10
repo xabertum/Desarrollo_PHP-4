5090a2f8a99751677a57bf622ee4d48939b31c04
xabertum <mohatar@gmail.com>
2018-05-10 12:02:33 +0200

    Minor changes

---
+++ ValidarUsuario.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Funcion que sirva para validar y limpiar un campo
+ *
+ * @param  $campo
+ * @return string
+ */
+function validar_campo ($campo)
+{
+    $campo = trim($campo);
+    $campo = stripcslashes($campo);
+    $campo = htmlspecialchars($campo);
+    
+    return $campo;
+}
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = validar_campo($_POST["username"]);
+    $password = validar_campo($_POST["password"]);
+    
+}
+   
