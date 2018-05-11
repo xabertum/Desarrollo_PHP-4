915c3424dbea92e66afc5466f4a98b93823c210b
xabertum <mohatar@gmail.com>
2018-05-11 13:06:48 +0200

    Minor changes

--- registro.php
+++ registro.php
@@ -13,26 +13,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     $username = validar_campo($_POST["username"]);
     $password = validar_campo($_POST["password"]);
-}
-
-
-/* Conectando al servidor de MySQL */
-$conn = new mysqli($host, $user, $passwordDB, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
-
-if ($conn->query($sql)) {
+    $password2 = validar_campo($_POST["password2"]);
+    
+    if ($password == $password2) {
+    
+    /* Conectando al servidor de MySQL */
+    $conn = new mysqli($host, $user, $passwordDB, $dbname);
+    
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    
+    $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+    
+    if ($conn->query($sql)) {
         
-    header('Location: index.html');
-    
-   } else {
-    echo 'failed '  . mysqli_error($conn) ;
+        header('Location: index.html');
+    } else {
+        echo 'failed ' . mysqli_error($conn);
+    }
+        
+    } else {
+        echo "<script>alert('Las contraseñas deben coincidir...');</script>";
+        
+    }
 }
-    
-    
 
 
