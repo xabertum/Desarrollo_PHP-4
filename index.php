71ca2ebdda68f3baacd3941847229bf03087128d
xabertum <mohatar@gmail.com>
2018-05-11 12:23:09 +0200

    Chnaged images size

--- index.php
+++ index.php
@@ -91,7 +91,26 @@
 			<div class="row">
 				<div class="col-md-4">
 					<div class="card mb-4 box-shadow">
-						<img src="images/1.jpg" class="card-img-top"
+						<img src="images/2.jpg" class="card-img-top"
+							data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail"
+							alt="Card image cap">
+						<div class="card-body">
+							<p class="card-text">This is a wider card with supporting text
+								below as a natural lead-in to additional content. This content
+								is a little bit longer.</p>
+							<div class="d-flex justify-content-between align-items-center">
+								<div class="btn-group">
+									<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+									<button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+								</div>
+								<small class="text-muted">9 mins</small>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card mb-4 box-shadow">
+						<img src="images/f2.jpg" class="card-img-top"
 							data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail"
 							alt="Card image cap">
 						<div class="card-body">
@@ -111,25 +130,6 @@
 				<div class="col-md-4">
 					<div class="card mb-4 box-shadow">
 						<img src="images/4.jpg" class="card-img-top"
-							data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail"
-							alt="Card image cap">
-						<div class="card-body">
-							<p class="card-text">This is a wider card with supporting text
-								below as a natural lead-in to additional content. This content
-								is a little bit longer.</p>
-							<div class="d-flex justify-content-between align-items-center">
-								<div class="btn-group">
-									<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-									<button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-								</div>
-								<small class="text-muted">9 mins</small>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="card mb-4 box-shadow">
-						<img src="images/5.jpg" class="card-img-top"
 							data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail"
 							alt="Card image cap">
 						<div class="card-body">
