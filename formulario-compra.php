2ccb1448bb8c32a6db0fa83c0dd3f938f24bfc82
xabertum <mohatar@gmail.com>
2018-05-14 23:36:49 +0200

    Minor changes

--- formulario-compra.php
+++ formulario-compra.php
@@ -1,40 +1,51 @@
 <?php
+include_once 'assets/connection.php';
+
+if (isset($_POST['datos_comprador'])) {
+    
+    $datos_comprador = $_POST['datos_comprador'];    
+    $toString = implode("','", $datos_comprador);       
+    
+    $sql = "INSERT INTO datos_comprador (nombre, apellidos, dni, telefono, direccion, email, profesion)
+            VALUES ('$toString')";
+    
+    $conn->query($sql);
+}
+
 ?>
 
 <!DOCTYPE doctype html>
 <html lang="es">
-    <head>
-        <meta charset="utf-8">
-            <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
-                <title>
-                    Signin Template for Bootstrap
-                </title>
-                <!-- Bootstrap core CSS -->
-                <link crossorigin="anonymous" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" rel="stylesheet">
-                    <!-- Custom styles for this template -->
-                    <link href="css/login-custom.css" rel="stylesheet">
-                    </link>
-                </link>
-            </meta>
-        </meta>
-    </head>
-    <body class="text-center">
-        <form action="ValidarUsuario.php" class="form-signin" method="POST">
-            <h1 class="h3 mb-3 font-weight-normal">
-                Please sign in
-            </h1>
-            <input autofocus="" class="form-control" name="username" placeholder="UserName" required="" type="text">
-                <input class="form-control" name="password" placeholder="Password" required="" type="password">
-                    <button class="btn btn-lg btn-primary btn-block" type="submit">
-                        Sign in
-                    </button>
-                    <p class="mt-5 mb-3 text-muted">
-                        © 2017-2018
-                    </p>
-                </input>
-            </input>
-        </form>
-    </body>
+<head>
+<meta charset="utf-8">
+<meta content="width=device-width, initial-scale=1, shrink-to-fit=no"
+	name="viewport">
+<title>Signin Template for Bootstrap</title>
+<!-- Bootstrap core CSS -->
+<link crossorigin="anonymous"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+	integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+	rel="stylesheet">
+<!-- Custom styles for this template -->
+<link href="css/login-custom.css" rel="stylesheet">
+
+</head>
+<body class="text-center">
+	<form action="<?php echo htmlentities($_SERVER['PHP_SELF']) ?>"
+		class="form-signin" method="POST">
+		<h1 class="h3 mb-3 font-weight-normal">Introduzca sus datos</h1>
+			<input autofocus class="form-control" name="datos_comprador[]" placeholder="Nombre" required type="text">
+			<input autofocus class="form-control" name="datos_comprador[]" placeholder="Apellidos" required type="text">
+			<input autofocus class="form-control" name="datos_comprador[]" placeholder="DNI" required type="text">
+			<input autofocus class="form-control" name="datos_comprador[]" placeholder="Telefono" required type="text">
+			<input autofocus class="form-control" name="datos_comprador[]" placeholder="Direccion" required type="text">
+			<input autofocus class="form-control" name="datos_comprador[]" placeholder="Correo electronico" required type="email">
+			<input autofocus class="form-control" name="datos_comprador[]" placeholder="Profesion" type="text">
+			<br>
+		<button class="btn btn-lg btn-primary" type="submit">Aceptar</button>
+	</form>
+
+</body>
 </html>
 
 
